14bf25f4f69040dba1c3dc62443c857174b1498e
Elia Maggioni <elia.maggioni@gmail.com>
2022-11-25 12:57:59 +0100

    test

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,7 @@ fn main() {
         "The answer to the ultimate question is {}",
         rust_template::answer()
     );
+    println!(
+        "What do you get when you multiply six by nine"
+    );
 }
